763d924808ffdc0b0d769d3bb5628f006c2d1b25
kristiansoerensen <75607273+kristiansoerensen@users.noreply.github.com>
2023-06-08 16:21:20 +0200

    [IMP] buttons

--- H3_embedded_programmeringsopgave.ino
+++ H3_embedded_programmeringsopgave.ino
@@ -1,80 +1,121 @@
-/* Simpel demo af OLED LCD Display med I2C */
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <EasyButton.h>
 #include "RTClib.h"
 #include "DHT.h"
-
-#define DHTPIN 2     // Digital pin connected to the DHT sensor
-#define DHTTYPE DHT11   // DHT 11
-// Initialize DHT sensor.
-DHT dht(DHTPIN, DHTTYPE);
 
 // ######## OLED ########
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET 4 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define OLED_RESET 5 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-// ###### OLED end ######
 
-// ######## RTC ########
+#define BTN1_PIN 2
+#define BTN2_PIN 3
+#define BTN3_PIN 4
+#define BTN4_PIN 5
+
+EasyButton btn1(BTN1_PIN);
+EasyButton btn2(BTN2_PIN);
+EasyButton btn3(BTN3_PIN);
+EasyButton btn4(BTN4_PIN);
+
+bool btn1Pressed = false;
+bool btn2Pressed = false;
+bool btn3Pressed = false;
+bool btn4Pressed = false;
+
+// Callback.
+void onbtn1Pressed() {
+  btn1Pressed = true;
+}
+void onbtn2Pressed() {
+  btn2Pressed = true;
+}
+void onbtn3Pressed() {
+  btn3Pressed = true;
+}
+void onbtn4Pressed() {
+  btn4Pressed = true;
+}
+
+int menuCounter = 0;
+bool inMainMenu = true;
+bool inClock = false;
+bool inDHT11 = false;
+bool inDHT11AndClock = false;
+bool inAdjustClock = false;
+
 RTC_DS3231 rtc;
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}; // Day of the week, that matches the index from the clock module
-// ###### RTC end ######
-
-
-// ## Other variables ##
-String myStr = "Hello, world!";
-
+int updateDatetimeDelay = 1000; // milli Seconds
+unsigned int lastDateTimeUpdate = 0;
 DateTime currentTime; // Stores the time read from the clock module
 
+#define DHTPIN 12     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11   // DHT 11
+// Initialize DHT sensor.
+DHT dht(DHTPIN, DHTTYPE);
+int updateDHTDelay = 1000; // milli Seconds
+unsigned int lastDHTUpdate = 0;
 float tempC;
 float humidity;
 
-int updateDatetimeDelay = 1000; // milli Seconds
-unsigned int lastDateTimeUpdate = 0;
-
-int updateDisplayDelay = 1000; // milli Seconds
-unsigned int lastDisplayUpdate = 0;
-
-int updateDHTDelay = 1000; // milli Seconds
-unsigned int lastDHTUpdate = 0;
-
+// Chenge between DHT11 and Clock
 int updateDisplayStateDelay = 3000; // milli Seconds
 unsigned int lastDisplayStateUpdate = 0;
 bool stateDisplay = false;
 
-void setup() 
-{
-	Serial.begin(9600);
+int updateDisplayDelay = 1000; // milli Seconds
+unsigned int lastDisplayUpdate = 0;
 
+void setup() {
   setupDisplay();
 
-  setupRTC();
+  // Setup Serial Monitor
+	Serial.begin(9600);
+  btn1.begin();
+  btn2.begin();
+  btn3.begin();
+  btn4.begin();
 
+  btn1.onPressed(onbtn1Pressed);
+  btn2.onPressed(onbtn2Pressed);
+  btn3.onPressed(onbtn3Pressed);
+  btn4.onPressed(onbtn4Pressed);
+
+  setupRTC();
   dht.begin();
 }
 
-void loop() 
-{
-  updateLastDateTime();
-  // serialPrintDateTime();
+void loop() {
+  btn1.read();
+  btn2.read();
+  btn3.read();
+  btn4.read();
 
+  updateLastDateTime();
   updateDHTData();
 
-	updateDisplay();
-}
-
-void setupDisplay(){
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-	if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) 
-	{ // Address for 128x64
-		Serial.println(F("SSD1306 allocation failed"));
-		for(;;); // Don't proceed, loop forever
-	}
+  // put your main code here, to run repeatedly:
+  if (inMainMenu){
+    displayMainManu();
+  }
+  else if (inClock){
+    displayClock();
+  }
+  else if (inDHT11){
+    displayDHT11();
+  }
+  else if (inDHT11AndClock){
+    displayDHT11AndClock();
+  }
+  else if (inAdjustClock){
+    displayAdjustClock();
+  }
 }
 
 void setupRTC(){
@@ -88,23 +129,10 @@ void setupRTC(){
   // rtc.adjust(DateTime(2023, 6, 7, 13, 4, 0));
 }
 
-void updateDisplay() {
-  if ((millis() - lastDisplayStateUpdate) > updateDisplayStateDelay) {
-    lastDisplayStateUpdate = millis();
-    stateDisplay = !stateDisplay;
-    Serial.println(stateDisplay);
-  }
-  if ((millis() - lastDisplayUpdate) > updateDisplayDelay) {
-    lastDisplayUpdate = millis();
-    if (stateDisplay) {
-      displayDateTime();
-    }
-    else {
-      displayDHTData();
-    }
-    // displayDateTime();
-
-    // displayDHTData();
+void updateLastDateTime() {
+  if ((millis() - lastDateTimeUpdate) > updateDatetimeDelay) {
+    lastDateTimeUpdate = millis();
+    currentTime = rtc.now();
   }
 }
 
@@ -116,14 +144,46 @@ void updateDHTData(){
   }
 }
 
-void updateLastDateTime() {
-  if ((millis() - lastDateTimeUpdate) > updateDatetimeDelay) {
-    lastDateTimeUpdate = millis();
-    currentTime = rtc.now();
+void displayClock() {
+  checkbtnClock();
+  display.clearDisplay();
+  char str[32];
+  displayTextCenter("Date", 0, 0, 1);
+  sprintf(str, "%02d/%02d/%02d", currentTime.year(), currentTime.month(), currentTime.day());
+  displayTextCenter(str, 0, 10, 2);
+  displayTextCenter("Time", 0, 35, 1);
+  sprintf(str, "%02d:%02d:%02d",  currentTime.hour(), currentTime.minute(), currentTime.second());
+  displayTextCenter(str, 0, 45, 2);
+  display.display();
+}
+
+void checkbtnClock(){
+  if (btn1Pressed)
+  {
+    Serial.println("Button1 was pressed");
+    inClock = false;
+    inMainMenu = true;
+    btn1Pressed = false;
+  }
+  if (btn2Pressed)
+  {
+    Serial.println("Button2 was pressed");
+    btn2Pressed = false;
+  }
+  if (btn3Pressed)
+  {
+    Serial.println("Button3 was pressed");
+    btn3Pressed = false;
+  }
+  if (btn4Pressed)
+  {
+    Serial.println("Button4 was pressed");
+    btn4Pressed = false;
   }
 }
 
-void displayDHTData(){
+void displayDHT11() {
+  checkbtnDHT11();
   display.clearDisplay();
   char str[] = "";
   displayTextCenter("Temperature", 0, 0, 1);
@@ -142,16 +202,181 @@ void displayDHTData(){
   display.display();
 }
 
-void displayDateTime(){
+void checkbtnDHT11(){
+  if (btn1Pressed)
+  {
+    Serial.println("Button1 was pressed");
+    inDHT11 = false;
+    inMainMenu = true;
+    btn1Pressed = false;
+  }
+  if (btn2Pressed)
+  {
+    Serial.println("Button2 was pressed");
+    btn2Pressed = false;
+  }
+  if (btn3Pressed)
+  {
+    Serial.println("Button3 was pressed");
+    btn3Pressed = false;
+  }
+  if (btn4Pressed)
+  {
+    Serial.println("Button4 was pressed");
+    btn4Pressed = false;
+  }
+}
+void displayDHT11AndClock() {
+  DHT11AndClock();
+  if ((millis() - lastDisplayStateUpdate) > updateDisplayStateDelay) {
+    lastDisplayStateUpdate = millis();
+    stateDisplay = !stateDisplay;
+    Serial.println(stateDisplay);
+  }
+  if ((millis() - lastDisplayUpdate) > updateDisplayDelay) {
+    lastDisplayUpdate = millis();
+    if (stateDisplay) {
+      displayClock();
+    }
+    else {
+      displayDHT11();
+    }
+  }
+}
+
+void DHT11AndClock(){
+  if (btn1Pressed)
+  {
+    Serial.println("Button1 was pressed");
+    inDHT11AndClock = false;
+    inMainMenu = true;
+    btn1Pressed = false;
+  }
+  if (btn2Pressed)
+  {
+    Serial.println("Button2 was pressed");
+    btn2Pressed = false;
+  }
+  if (btn3Pressed)
+  {
+    Serial.println("Button3 was pressed");
+    btn3Pressed = false;
+  }
+  if (btn4Pressed)
+  {
+    Serial.println("Button4 was pressed");
+    btn4Pressed = false;
+  }
+}
+void displayAdjustClock() {
+  checkAdjustClock();
   display.clearDisplay();
-  char str[32];
-  displayTextCenter("Date", 0, 0, 1);
-  sprintf(str, "%02d/%02d/%02d", currentTime.year(), currentTime.month(), currentTime.day());
-  displayTextCenter(str, 0, 10, 2);
-  displayTextCenter("Time", 0, 35, 1);
-  sprintf(str, "%02d:%02d:%02d",  currentTime.hour(), currentTime.minute(), currentTime.second());
-  displayTextCenter(str, 0, 45, 2);
+  display.setTextSize(1); // Normal 1:1 pixel scale
+	display.setTextColor(WHITE); // Draw white text
+  display.setCursor(10,20);
+  display.println("displayAdjustClock");
   display.display();
+}
+
+void checkAdjustClock(){
+  if (btn1Pressed)
+  {
+    Serial.println("Button1 was pressed");
+    inAdjustClock = false;
+    inMainMenu = true;
+    btn1Pressed = false;
+  }
+  if (btn2Pressed)
+  {
+    Serial.println("Button2 was pressed");
+    btn2Pressed = false;
+  }
+  if (btn3Pressed)
+  {
+    Serial.println("Button3 was pressed");
+    btn3Pressed = false;
+  }
+  if (btn4Pressed)
+  {
+    Serial.println("Button4 was pressed");
+    btn4Pressed = false;
+  }
+}
+
+void setupDisplay(){
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+	if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) 
+	{ // Address for 128x64
+		Serial.println(F("SSD1306 allocation failed"));
+		for(;;); // Don't proceed, loop forever
+	}
+}
+
+void displayMainManu(){
+  checkbtnMainMenu();
+  display.clearDisplay();
+  displayTextCenter("Main Menu", 0, 0, 2);
+
+  display.setTextSize(1); // Normal 1:1 pixel scale
+	display.setTextColor(WHITE); // Draw white text
+  display.setCursor(10,20);
+  display.print("Clock");
+  display.setCursor(10,30);
+  display.print("DHT11");
+  display.setCursor(10,40);
+  display.print("Clock & DHT11");
+  display.setCursor(10,50);
+  display.print("Adjust clock");
+
+  display.setCursor(2, (menuCounter * 10) + 20);
+  display.print(">");
+  display.display();
+}
+
+void checkbtnMainMenu(){
+  if (btn1Pressed)
+  {
+    Serial.println("Button1 was pressed");
+    menuCounter++;
+    if (menuCounter > 3){
+      menuCounter=0;
+    }
+    btn1Pressed = false;
+  }
+  if (btn2Pressed)
+  {
+    Serial.println("Button2 was pressed");
+    if (menuCounter == 0){
+      inClock = true;
+      inMainMenu = false;
+    }
+    else if (menuCounter == 1) {
+      inDHT11 = true;
+      inMainMenu = false;
+    }
+    else if (menuCounter == 2) {
+      inDHT11AndClock = true;
+      inMainMenu = false;
+    }
+    else if (menuCounter == 3) {
+      inAdjustClock = true;
+      inMainMenu = false;
+    }
+    else {
+      inMainMenu = true;
+    }
+    btn2Pressed = false;
+  }
+  if (btn3Pressed)
+  {
+    Serial.println("Button3 was pressed");
+    btn3Pressed = false;
+  }
+  if (btn4Pressed)
+  {
+    Serial.println("Button4 was pressed");
+    btn4Pressed = false;
+  }
 }
 
 void displayTextCenter(char str[], int x, int y, int textSize){
@@ -169,52 +394,4 @@ int getOffset(char str[], int *width, int *textSize){
   // then we divide by 2, to get the middle of the word, and then we extract that
   // from the screen area, we extract the string middle, and get the offset.
   return ((SCREEN_WIDTH - *width) / 2 ) - (strlen(str)*(6*(*textSize)) / 2);
-}
-
-void serialPrintDateTime(){
-  DateTime now = currentTime;
-
-  Serial.print(now.year(), DEC);
-  Serial.print('/');
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.day(), DEC);
-  Serial.print(" (");
-  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-  Serial.print(") ");
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  Serial.print(now.second(), DEC);
-  Serial.println();
-
-  Serial.print(" since midnight 1/1/1970 = ");
-  Serial.print(now.unixtime());
-  Serial.print("s = ");
-  Serial.print(now.unixtime() / 86400L);
-  Serial.println("d");
-
-  // calculate a date which is 7 days, 12 hours, 30 minutes, 6 seconds into the future
-  DateTime future (now + TimeSpan(7,12,30,6));
-
-  Serial.print(" now + 7d + 12h + 30m + 6s: ");
-  Serial.print(future.year(), DEC);
-  Serial.print('/');
-  Serial.print(future.month(), DEC);
-  Serial.print('/');
-  Serial.print(future.day(), DEC);
-  Serial.print(' ');
-  Serial.print(future.hour(), DEC);
-  Serial.print(':');
-  Serial.print(future.minute(), DEC);
-  Serial.print(':');
-  Serial.print(future.second(), DEC);
-  Serial.println();
-
-  Serial.print("Temperature: ");
-  Serial.print(rtc.getTemperature());
-  Serial.println(" C");
-
-  Serial.println();
 }
